bc1773514638b5bb031803213cd61503c5738279
LizethQuispeCode <lizeth.quispe@vallegrande.edu.pe>
2026-07-02 14:00:51 -0500

    Add LicensePlateDTO and LicensePlateDetailDTO classes

--- backend/src/main/java/com/hackathon/backend/dto/StudentDTO.java
+++ backend/src/main/java/com/hackathon/backend/dto/StudentDTO.java
@@ -16,3 +16,47 @@ public class StudentDTO {
     private String dni;
     private Boolean isActive;
 }
+
+
+
+
+package com.hackathon.backend.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class LicensePlateDTO {
+
+    private Integer licensePlatesId;
+    private String orderCode;
+    private Integer studentId;
+    private Integer raceId;
+    private Integer campusId;
+    private String promoter;
+    private LocalDate purchaseDate;
+    private Boolean isActive;
+}
+
+
+
+package com.hackathon.backend.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class LicensePlateDetailDTO {
+
+    private Integer licensePlatesId;
+    private String orderCode;
+
+    private String studentName;
+    private String raceName;
+    private String campusName;
+
+    private String promoter;
+    private LocalDate purchaseDate;
+}
