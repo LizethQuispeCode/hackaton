555a76b7c038ce1a7111a28d491eac4e51f14024
LizethQuispeCode <lizeth.quispe@vallegrande.edu.pe>
2026-07-02 14:20:37 -0500

    Add Student model file

---
+++ hackathon-frontend/src/app/core/models/Student.ts
@@ -0,0 +1,22 @@
+export interface Student {
+
+  studentId?: number;
+
+  name: string;
+  lastName: string;
+  email: string;
+  address?: string;
+
+  age: number;
+  dni: string;
+
+  isActive?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+
+
+
+
+
